a4c5aadf59c43ed22b6c53e506493aa81ee2bb89
rajkumar-t03 <rajkumar79061@gmail.com>
2024-12-29 12:21:34 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
+import { inter } from '@/app/ui/fonts.ts';
  
-export const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
